14cde57be0e7c00bc0f19b46a20c1a8f53c91d5b
vladya905 <lubajkinvlad@gmail.com>
2023-08-02 21:22:31 +0300

    create function updateContact

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,68 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+import exp from "constants";
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("db", "contacts.json")
+const updateContactsStorage = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-const getContactById = async (contactId) => {}
+ 
+export const listContacts = async() => {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+ 
+export const getContactById = async(contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(item => item.id === contactId)
+  return result || null
 
-const addContact = async (body) => {}
+}
 
-const updateContact = async (contactId, body) => {}
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId)
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await updateContactsStorage(contacts)
+  return result;
+  
+}
 
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+export const addContact = async ({ name, email, phone }) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone
+  }
+  contacts.push(newContact);
+  await updateContactsStorage(contacts)
+  return newContact;
+  
+}
+
+export const updateContact = async (contactId, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === contactId);
+  if (index === -1) {
+    return null
+  }
+  contacts[index] = { name, email, phone };
+  await updateContactsStorage(contacts)
+  return contacts[index]
+    
+}
+
+
+export default {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
 }
